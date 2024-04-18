6e9bf641a5baacd5f92438610bd527194597d651
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-04-18 18:04:01 +0400

    task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+'''
+task 5
+'''
+
+from flask import Flask, render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    return f'C {escape(text).replace("_", " ")}'
+
+
+@app.route("/python", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_is_fun(text):
+    return f'Python {escape(text).replace("_", " ")}'
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f'{escape(n)} is a number'
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def num_temp(n):
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=False)
