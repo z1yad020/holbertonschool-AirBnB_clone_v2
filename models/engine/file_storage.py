bc517300f1b8c4b7d7452859457a4cd0071d53d2
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-04-19 12:43:55 +0400

    task 8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,3 +69,7 @@ class FileStorage:
                     self.all()[key] = classes[val["__class__"]](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """deserializing the JSON file to objects"""
+        self.reload()
---
+++ web_flask/7-states_list.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+'''
+task 7
+'''
+
+from models import storage
+from flask import Flask, render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    return f'C {escape(text).replace("_", " ")}'
+
+
+@app.route("/python", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_is_fun(text):
+    return f'Python {escape(text).replace("_", " ")}'
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f'{escape(n)} is a number'
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def num_temp(n):
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def num_o_even(n):
+    return render_template("6-number_odd_or_even.html",
+                           e_o="odd" if n % 2 else "even", n=n)
+
+
+@app.teardown_appcontext
+def cl(random):
+    storage.close()
+
+
+@app.route("/state_list", strict_slashes=False)
+def state_list():
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template("7-states_list.html", states=sorted_states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=False)
